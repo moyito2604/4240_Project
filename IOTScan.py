b1a57b301a5544079e715968cafb396f9c6dc823
Mobro <39844165+moyito2604@users.noreply.github.com>
2024-04-05 15:45:04 -0400

    Testing Changes

--- IOTScan.py
+++ IOTScan.py
@@ -1,5 +1,23 @@
+import pyshark
 import os
 
 interface = os.environ.get("tshark_int")
+sourceip = os.environ.get("tshark_srcip", False)
+#
+# command = f"tshark -i {interface}"
+# if sourceip:
+#     command = command + f" -e ip.src -Y \"ip.src=={sourceip}"
+# print(f"Tshark Command: {command}")
+# os.system(command)
 
-os.system(f"tshark -i {interface}")
+interface='wlx984827c0b944'
+sourceip='198.21.252.164'
+
+capture = pyshark.LiveCapture(interface=interface, bpf_filter=f"ip host {sourceip}")
+print("Retrieving Packets")
+#capture.sniff(timeout=5)
+# capture.sniff_continuously(packet_count=5)
+
+#print(capture[0])
+for packet in capture.sniff_continuously(packet_count=1000):
+    print(packet.ip)
---
+++ docker-build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker build -t ghcr.io/moyito2604/iotscan:latest .
