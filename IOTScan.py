a01ba2063d6fb5f820fd0fec843e97cb17b92c54
Mobro <39844165+moyito2604@users.noreply.github.com>
2024-04-07 17:22:10 -0400

    Reverse DNS lookup added

--- IOTScan.py
+++ IOTScan.py
@@ -11,6 +11,12 @@ sourceip = os.environ.get("SOURCEIP", False)
 # Amount of packets to grab
 packetcnt = os.environ.get("PACKETCNT", False)
 
+# Amount of calls to domain
+
+interface='wlx984827c0b944'
+sourceip='10.42.0.130'
+packetcnt=1000
+
 if not interface or not sourceip or not packetcnt:
     print("INTERFACE, SOURCEIP, and PACKETCNT environment variables required.")
     exit(0)
@@ -19,4 +25,10 @@ capture = pyshark.LiveCapture(interface=interface, bpf_filter=f"ip host {sourcei
 print("Retrieving Packets")
 
 for packet in capture.sniff_continuously(packet_count=packetcnt):
-    print(f"{packet.__dict__['number']}".ljust(7), f"{packet.ip.src} --> {packet.ip.dst}", f"{packet.__dict__['layers']}")
+    if packet.ip.src == sourceip:
+        print(f"{packet.__dict__['number']}".ljust(7),
+              f'{packet.ip.src} --> {socket.getnameinfo((packet.ip.dst, 0), 0)[0]}', f"{packet.__dict__['layers']}")
+    else:
+        print(f"{packet.__dict__['number']}".ljust(7),
+              f'{socket.getnameinfo((packet.ip.src, 0), 0)[0]} --> {packet.ip.dst}', f"{packet.__dict__['layers']}")
+
